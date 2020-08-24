350faf2cb44ced4ddb6902dc9c0f61738eeca210
stiftcast <stiftcast@gmail.com>
2020-08-23 23:38:02 -0400

    Update __init__.py

--- timefops/__init__.py
+++ timefops/__init__.py
@@ -1,9 +1,6 @@
-__author__ == "stiftcast"
-__license__ == "GPLv3"
-__version__ == "0.2"
-
-TRANSLATIONS = {"atime": "access-time",
-                "ctime": "change-time",
-                "mtime": "modified-time"}
+__all__ = ["archive", "copy", "move"]
+__author__ = "stiftcast"
+__license__ = "GPLv3"
+__version__ = "0.2"
 
 from .timefops import archive, copy, move
