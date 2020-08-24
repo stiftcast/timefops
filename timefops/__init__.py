e75d7f420a15900214d5246e621108860e78ddb1
stiftcast <stiftcast@gmail.com>
2020-08-23 22:37:51 -0400

    add translations

--- timefops/__init__.py
+++ timefops/__init__.py
@@ -2,4 +2,8 @@ __author__ == "stiftcast"
 __license__ == "GPLv3"
 __version__ == "0.2"
 
+TRANSLATIONS = {"atime": "access-time",
+                "ctime": "change-time",
+                "mtime": "modified-time"}
+
 from .timefops import archive, copy, move
